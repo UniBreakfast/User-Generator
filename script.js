327f3e4b113dc0d768e99d5fbd6874de4515cff7
bloon1k <goncharov.vadn@gmail.com>
2022-01-12 21:16:42 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const btn = document.getElementsByClassName("gen")[0];
+const out = document.getElementById("out");
+
+let users = "Микита, Сергій, Дмитро, Ольга, Ганна, Юлія, Вікторія, Євгеній, Єгор, Артем, Віталій, Назар".split(", ");
+let lastPicks = JSON.parse(localStorage.getItem("lastPicks"));
+
+btn.onclick = pickUser;
+
+function genRnd(limit) {
+  return Math.floor(Math.random() * limit);
+}
+function pickUser() {
+  let selectedUser;
+  do {
+    selectedUser = users[genRnd(users.length)];
+  } while (lastPicks.includes(selectedUser))
+  out.value = selectedUser;
+  lastPicks.length = 2;
+  lastPicks.unshift(selectedUser);
+  console.log(lastPicks);
+  localStorage.setItem("lastPicks", JSON.stringify(lastPicks));
+}
